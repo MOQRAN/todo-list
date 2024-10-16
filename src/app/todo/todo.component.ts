274d67e5c2f79ce542209fe500f74f5d275b0ef2
MOQRAN <moqran.md@gmail.com>
2024-10-16 12:01:01 +0200

    todo component

---
+++ src/app/todo/todo.component.ts
@@ -0,0 +1,27 @@
+import { Component } from '@angular/core';
+import { TodoService } from '../todo.service';
+
+@Component({
+  selector: 'app-todo',
+  templateUrl: './todo.component.html',
+  styleUrls: ['./todo.component.css']
+})
+export class TodoComponent {
+  newTodo: string = '';
+  todos: string[] = [];
+
+  constructor(private todoService: TodoService) {
+    this.todos = this.todoService.getTodos();
+  }
+
+  addTodo() {
+    if (this.newTodo.trim()) {
+      this.todoService.addTodo(this.newTodo.trim());
+      this.newTodo = '';
+    }
+  }
+
+  deleteTodo(index: number) {
+    this.todoService.deleteTodo(index);
+  }
+}

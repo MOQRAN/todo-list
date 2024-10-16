dd80d560f2d73da9d0ec7918cc8cf94cbc892c0f
MOQRAN <moqran.md@gmail.com>
2024-10-16 15:18:42 +0200

    adding style and animations

--- src/app/todo/todo.component.ts
+++ src/app/todo/todo.component.ts
@@ -19,6 +19,15 @@ import { trigger, state, style, transition, animate } from '@angular/animations'
       transition(':leave', [
         animate('300ms ease-out', style({ opacity: 0, transform: 'translateY(-20px)' }))
       ])
+    ]),
+    trigger('dropdownAnimation', [
+      transition(':enter', [
+        style({ opacity: 0, transform: 'translateY(-10px)' }),
+        animate('200ms ease-out', style({ opacity: 1, transform: 'translateY(0)' }))
+      ]),
+      transition(':leave', [
+        animate('200ms ease-out', style({ opacity: 0, transform: 'translateY(-10px)' }))
+      ])
     ])
   ],
 })
